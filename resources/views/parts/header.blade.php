023a291bed2685419e26c9ab825b1154396df699
Daniël <19392499+DJ-Daniel@users.noreply.github.com>
2021-11-16 15:35:05 +0100

    Update header.blade.php

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -13,7 +13,7 @@
           <li><a class="nav-link  {{ (request()->is('reserveren')) ? 'active' : '' }}" href="/reserveren">Baan reserveren</a></li>
           <li><a class="nav-link {{ (request()->is('tournooien')) ? 'active' : '' }}" href="/tournooien">Tournooien</a></li>
           @endif
-          <li><a class="nav-link" href="/menu">Menu kaart</a></li>
+          <li><a class="nav-link {{ (request()->is('menu')) ? 'active' : '' }}" href="/menu">Menu kaart</a></li>
           {{-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
