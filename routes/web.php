8bfa15cafb20926c6982e882c5efea8627b17d95
Daniël <19392499+DJ-Daniel@users.noreply.github.com>
2021-11-17 15:36:35 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware(['auth'])->group(function () {
 // Admin only
 Route::get('/admin/home', 'AdminController@index')->name('adminIndex');
 Route::get('/admin/users', 'AdminController@users')->name('adminUsers');
-Route::get('/admin/edit', 'AdminController@edit')->name('adminedit');
+Route::get('/admin/users/edit', 'AdminController@edit')->name('adminedit');
 
 // Redirects
 Route::redirect('/admin', '/admin/home');
