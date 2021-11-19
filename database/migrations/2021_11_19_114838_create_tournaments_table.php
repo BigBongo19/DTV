0045bf9f1ee1baccc6610ddeed0f64b2a4e39646
Daniël <19392499+DJ-Daniel@users.noreply.github.com>
2021-11-19 13:01:28 +0100

    Added migrations

---
+++ database/migrations/2021_11_19_114838_create_tournaments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTournamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('max_participants');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->string('img_path');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tournaments');
+    }
+}
