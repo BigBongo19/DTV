148dd5d9c5e54925e41b78575ff7a954bdcd004b
Daniël <19392499+DJ-Daniel@users.noreply.github.com>
2021-11-23 13:13:24 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/edit-profile/submit', 'ProfileController@edit');
     Route::post('/edit-profile/submitpassword', 'ProfileController@editPassword');
     Route::get('/payments', 'ProfileController@payment')->name('paymentIndex');
-    Route::get('/reserveren', 'ReserveController@index')->name('index');
     Route::get('/toernooien', 'TournamentController@getTournaments')->name('index');
     Route::get('/toernooi', 'TournamentDetailController@index')->name('index');
 
