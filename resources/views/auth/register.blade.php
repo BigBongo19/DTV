3cf9707cb0d34fd77ab9327bf0dc65ad8f4dc150
BigBongo17 <tycho.alternative@gmail.com>
2021-11-24 14:45:16 +0100

    Aanpassing design login/registeren

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,86 +1,108 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<title>Login V1</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
+    <title>Login V1</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
+
 <body>
+    <!-- ======= Header ======= -->
+    @include('parts.header')
+    <!-- End Header -->
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-pic">
+                    <h1><b>Creeër een account</b> voor DTV</h1>
+                    <img src="images/tennis_people.jpg" alt="IMG">
+                </div>
 
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/img-01.png" alt="IMG">
-				</div>
-
-                    <form class="login100-form validate-form" method="POST" action="{{ route('register') }}">
+                <form class="login100-form validate-form" method="POST" action="{{ route('register') }}">
                     @csrf
-					<span class="login100-form-title">
-						Registeren
-					</span>
-					<div class="wrap-input100 validate-input" data-validate = "Naam is verplicht">
-						<input class="input100" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"  autocomplete="name" placeholder="Naam" data-validate = "Je moet een naam invullen">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-user" aria-hidden="true"></i>
-						</span>
-					</div>
-                    <div class="wrap-input100 validate-input" data-validate = "Achternaam is verplicht">
-						<input class="input100" id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}"  autocomplete="last_name" placeholder="Achternaam" data-validate = "Je moet een achternaam invullen">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-user" aria-hidden="true"></i>
-						</span>
-					</div>
-					<div class="wrap-input100 validate-input" data-validate = "Email is verplicht">
-						<input class="input100" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  autocomplete="email" placeholder="Email" data-validate = "Je moet een Email invullen">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
-						</span>
-					</div>
+                    <span class="login100-form-title">
+                        Registreer om lid te worden van DTV
+                    </span>
+                    <div class="wrap-input100 validate-input" data-validate="Email is verplicht">
+                        <input class="input100" id="email" type="email"
+                            class="form-control @error('email') is-invalid @enderror" name="email"
+                            value="{{ old('email') }}" autocomplete="email" placeholder="Email"
+                            data-validate="Je moet een Email invullen">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                    <div class="name-wrapper d-flex">
+                        <div class="wrap-input100 validate-input mr-3" data-validate="Naam is verplicht">
+                            <input class="input100" id="name" type="text"
+                                class="form-control @error('name') is-invalid @enderror" name="name"
+                                value="{{ old('name') }}" autocomplete="name" placeholder="Naam"
+                                data-validate="Je moet een naam invullen">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                        <div class="wrap-input100 validate-input ml-3" data-validate="Achternaam is verplicht">
+                            <input class="input100" id="last_name" type="text"
+                                class="form-control @error('last_name') is-invalid @enderror" name="last_name"
+                                value="{{ old('last_name') }}" autocomplete="last_name" placeholder="Achternaam"
+                                data-validate="Je moet een achternaam invullen">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                    </div>
 
+                    <div class="wrap-input100 validate-input" data-validate="Wachtwoord is verplicht">
+                        <input class="input100" id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password"
+                            autocomplete="current-password" placeholder="Wachtwoord"
+                            data-validate="Je moet een wachtwoord invullen">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Wachtwoord is verplicht">
-						<input class="input100" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"  autocomplete="current-password" placeholder="Wachtwoord" data-validate = "Je moet een wachtwoord invullen">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-lock" aria-hidden="true"></i>
-						</span>
-					</div>
-
-                    <div class="wrap-input100 validate-input" data-validate = "Wachtwoord herhalen is verplicht">
-						<input class="input100" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password_confirmation"  autocomplete="current-password" placeholder="Wachtwoord bevestigen" data-validate = "Je moet een wachtwoord invullen">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-lock" aria-hidden="true"></i>
-						</span>
-					</div>
-					<div class="container-login100-form-btn">
+                    <div class="wrap-input100 validate-input" data-validate="Wachtwoord herhalen is verplicht">
+                        <input class="input100" id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password_confirmation"
+                            autocomplete="current-password" placeholder="Wachtwoord bevestigen"
+                            data-validate="Je moet een wachtwoord invullen">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                    <div class="container-login100-form-btn">
                         <button type="submit" class="login100-form-btn">
                             {{ __('Registeren') }}
                         </button>
-					</div>
+                    </div>
 
-					{{-- <div class="text-center p-t-12">
+                    {{-- <div class="text-center p-t-12">
 						<span class="txt1">
 							Forgot
 						</span>
@@ -89,36 +111,37 @@
 						</a>
 					</div> --}}
 
-					<div class="text-center p-t-20">
-						<a class="txt2" href="/login">
-							Inloggen
-							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-						</a>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+                    <div class="text-center p-t-20">
+                        <a class="txt2" href="/login">
+                            Inloggen
+                            <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 
 
 
-<!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/tilt/tilt.jquery.min.js"></script>
-	<script >
-		$('.js-tilt').tilt({
-			scale: 1.1
-		})
-	</script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/tilt/tilt.jquery.min.js"></script>
+    <script>
+        $('.js-tilt').tilt({
+            scale: 1.1
+        })
+    </script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
 
 </body>
+
 </html>
