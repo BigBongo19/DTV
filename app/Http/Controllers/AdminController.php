674509737747b10e523941e75a2b0a89ed2c2eac
BigBongo17 <tycho.alternative@gmail.com>
2021-11-30 15:55:36 +0100

    updates

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,10 +8,8 @@ use App\Tournament;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Reservation;
-use App\Tournament;
 use App\Court;
 use App\User;
 use App\Menu;
