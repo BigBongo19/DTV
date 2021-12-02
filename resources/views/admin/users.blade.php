0ac3e709c5a84fc70951e591d9ff00ee6a268f6f
tyler1 <job.mekkelholt@student.graafschapcollege.nl>
2021-12-02 13:53:24 +0100

    push

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DTV Admin | Home</title>
+    <title>DTV Admin | Gebruikers</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
